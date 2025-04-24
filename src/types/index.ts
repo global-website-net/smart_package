a393ed8a629f8ab0ba047264d9aa97c1664c6338
yousef karram <globalwebsitenet@gmail.com>
2025-04-24 10:39:25 +0300

    Fix functionality of header/bottom - 3

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Package {
+  id: string
+  trackingNumber: string
+  status: string
+  shop: string
+  currentLocation: string
+  userId: string
+  createdAt: string
+  updatedAt: string
+} 
