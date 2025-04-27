9b7db6f08e988ddb86fdc7b7f432c0ac4e43871a
yousef karram <globalwebsitenet@gmail.com>
2025-04-27 13:11:07 +0300

    Fix design of buttons in pages - 33

--- src/app/auth/reset-password/[token]/page.tsx
+++ src/app/auth/reset-password/[token]/page.tsx
@@ -1,17 +1,16 @@
 import ResetPasswordForm from '@/components/ResetPasswordForm'
 import { Metadata } from 'next'
 
-type Props = {
-  params: { token: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
-
 export const metadata: Metadata = {
   title: 'إعادة تعيين كلمة المرور',
   description: 'إعادة تعيين كلمة المرور الخاصة بك',
 }
 
-export default async function ResetPasswordPage(props: Props) {
+export default async function ResetPasswordPage({
+  params,
+}: {
+  params: { token: string }
+}) {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -24,7 +23,7 @@ export default async function ResetPasswordPage(props: Props) {
           </p>
         </div>
 
-        <ResetPasswordForm token={props.params.token} />
+        <ResetPasswordForm token={params.token} />
       </div>
     </div>
   )
