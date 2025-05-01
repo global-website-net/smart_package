c67d702fa1a52d8094ae4e94fb6c51a875ce770b
yousef karram <globalwebsitenet@gmail.com>
2025-05-01 11:17:32 +0300

    date 1 May - 7

--- src/app/api/auth/reset-password/[token]/route.ts
+++ src/app/api/auth/reset-password/[token]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 import bcrypt from 'bcryptjs'
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { token: string } }
 ) {
   try {
@@ -37,7 +37,7 @@ export async function GET(
 }
 
 export async function POST(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { token: string } }
 ) {
   try {
