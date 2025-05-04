22e7e39029fb0bbc6f7cb785fe510843911319c2
yousef karram <globalwebsitenet@gmail.com>
2025-05-04 15:25:20 +0300

    date 4 May - 27

--- src/app/api/orders/[orderId]/route.ts
+++ src/app/api/orders/[orderId]/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { supabase } from '@/lib/supabase'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/auth.config'
 
 export async function PATCH(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { orderId: string } }
 ) {
   try {
